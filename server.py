4588f2b4a229001bf2cd35f275dad92b9ef68a0e
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-06-30 08:27:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,18 @@
-import socket   
+import socket
+#from tkinter import *
+
+#client.send(chat.encode('utf-8'))
+connect_cout = 0
 
 
+
+#Connect
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(('192.168.0.50', 6743))
+client.connect(('10.80.68.86', 4783))
 
 while True:
-
-    command = input('''
-    
-    
-''')
+    client.send(input().encode('utf-8'))
 
 
-    if command == '/'[-1]:
-        client.send('ура'.encode('utf-8'))
 
-    client.send(command.encode('utf-8'))
