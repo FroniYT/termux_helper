dd5e09305474966b1cbaf01f37c7a411271d1e67
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-06-30 14:35:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
     
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(('10.120.44.54',1488))
+server.bind(('192.168.0.50',1488))
 server.listen()
 
 while True:
