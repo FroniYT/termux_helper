3e50ad94a83de1d98f4f63f7f7fcc612a7cf3af2
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-06-30 07:43:37 +0300

    Update termux.py

--- termux.py
+++ termux.py
@@ -1,10 +1,12 @@
-import socket   
+import socket
+#from tkinter import *
+
+#client.send(chat.encode('utf-8'))
 
 
+#Connect
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(('192.168.0.50', 6743))
+client.connect(('10.80.68.86', 4783))
 
-while True:
-
-    client.send(input().encode('utf-8'))
+client.send(input().encode('utf-8'))
