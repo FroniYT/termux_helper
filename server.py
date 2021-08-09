a0715ea99ff891617fe7dbfb8f1ef8d1ca04359b
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-08-09 19:06:42 +0700

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,5 @@ server_socket.bind(("0.0.0.0", 8000))
 server_socket.listen(1)
 (client_socket, client_address) = server_socket.accept()
 while True:
-    print ("client_connected [" + client_address[0] + "]")
     client_command = client_socket.recv(1024).decode()
     print(client_command)
