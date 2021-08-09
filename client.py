b98f54e4ccd1a141de457e5808def2869ba12c27
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-08-09 19:11:55 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 client_socket = socket.socket()
-client_socket.connect(("10.128.29.226", 8000)) #or enter ip of server
+client_socket.connect(("192.168.56.1", 8000)) #or enter ip of server
 while True:
     client_command = input("command: ")
     client_socket.send(client_command.encode())
