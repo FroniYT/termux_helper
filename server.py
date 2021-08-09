1c897f9cee2d4cfbe4518961158834b1bd2af254
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-08-09 19:13:03 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 server_socket = socket.socket()
-server_socket.bind(("0.0.0.0", 8000))
+server_socket.bind(("192.168.56.1", 8000))
 server_socket.listen(1)
 (client_socket, client_address) = server_socket.accept()
 while True:
