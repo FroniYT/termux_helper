15ca4b66721ccca2fa18d41ad3cf9e901c7950d0
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-08-17 14:06:05 +0700

    Delete server.py

--- server.py
+++
@@ -1,8 +0,0 @@
-import socket
-server_socket = socket.socket()
-server_socket.bind(("192.168.56.1", 8000))
-server_socket.listen(1)
-(client_socket, client_address) = server_socket.accept()
-while True:
-    client_command = client_socket.recv(1024).decode()
-    print(client_command)
