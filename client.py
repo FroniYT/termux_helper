5e880c997d20b96df8acd38d894713d27d3d1d36
FroniYT <71455164+FroniYT@users.noreply.github.com>
2021-08-17 14:05:56 +0700

    Delete client.py

--- client.py
+++
@@ -1,6 +0,0 @@
-import socket
-client_socket = socket.socket()
-client_socket.connect(("192.168.56.1", 8000)) #or enter ip of server
-while True:
-    client_command = input("command: ")
-    client_socket.send(client_command.encode())
